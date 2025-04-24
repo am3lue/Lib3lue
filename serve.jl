49be271945f029a5cb54c88e000149e913d2ef56
Blue francis <am3lue@gmail.com>
2025-04-24 16:06:00 +0300

    ip address changed

--- serve.jl
+++ serve.jl
@@ -1,5 +1,9 @@
 using Genie
 using Pkg
+using Sockets
+
+# Clear screen
+run(`clear`)
 
 # Activate the environment
 Pkg.activate(".")
@@ -12,12 +16,22 @@ try
     # Configure server settings
     Genie.config.run_as_server = true
     Genie.config.server_port = 8000
-    Genie.config.server_host = "127.0.0.1"
+    Genie.config.server_host = "0.0.0.0"
     Genie.config.cors_headers["Access-Control-Allow-Origin"] = "*"
     Genie.config.cors_headers["Access-Control-Allow-Headers"] = "Content-Type"
     Genie.config.cors_headers["Access-Control-Allow-Methods"] = "GET, POST, PUT, DELETE, OPTIONS"
     
+    # Get local IP addresses
+    local_ips = getipaddrs()
+    println("\nAvailable IP addresses:")
+    for ip in local_ips
+        if !startswith(string(ip), "127.") && !startswith(string(ip), "::")
+            println("  - http://$ip:$(Genie.config.server_port)")
+        end
+    end
+    
     println("""
+    
     Starting 3lue Library server...
     Server running at http://$(Genie.config.server_host):$(Genie.config.server_port)
     Press Ctrl+C to stop the server
